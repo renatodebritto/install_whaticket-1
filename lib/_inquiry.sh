9af08112c6b2577a90ba4009df1d99fc7ad32085
Jorge Luiz gomes <52924232+jjluizgomes@users.noreply.github.com>
2022-07-04 18:36:19 -0300

    Update _inquiry.sh

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -97,8 +97,8 @@ inquiry_options() {
   print_banner
   printf "${WHITE} 💻 Bem vindo(a) ao instalador Jorge Zap, Selecione abaixo a proxima ação!${GRAY_LIGHT}"
   printf "\n\n"
-  printf "   [1] Instalar o OwenZap\n"
-  printf "   [2] Atualizar o OwenZap\n"
+  printf "   [1] Instalar o Jorgezap\n"
+  printf "   [2] Atualizar o Jorgezap\n"
   printf "\n"
   read -p "> " option
 
