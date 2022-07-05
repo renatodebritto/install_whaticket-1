c6d3d8c289d3478236e454de09993de9f9ff340c
Jorge Luiz gomes <52924232+jjluizgomes@users.noreply.github.com>
2022-07-05 00:49:42 -0300

    Update _inquiry.sh

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -7,15 +7,6 @@ get_mysql_root_password() {
   printf "\n\n"
   read -p "> " mysql_root_password
 }
-
-get_link_git() {
-  
-  print_banner
-  printf "${WHITE} 💻 Insira o link do GITHUB do sistema:${GRAY_LIGHT}"
-  printf "\n\n"
-  read -p "> " link_git
-}
-
 get_instancia_add() {
   
   print_banner
