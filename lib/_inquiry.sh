1db4ed5ef7857ade86f2c2c3bbe26201187b506a
Jorge Luiz gomes <52924232+jjluizgomes@users.noreply.github.com>
2022-07-05 21:07:53 -0300

    Update _inquiry.sh

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -1,55 +1,102 @@
 #!/bin/bash
 
+get_mysql_root_password() {
+  
+  print_banner
+  printf "${WHITE} 💻 Insira senha padrão para o sistema:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " mysql_root_password
+}
+
+get_link_git() {
+  
+  print_banner
+  printf "${WHITE} 💻 Insira o link do GITHUB do sistema:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " link_git
+}
+
 get_instancia_add() {
   
   print_banner
-  printf "${WHITE} 💻 Digite o nome da instancia a ser instalada (Utilizar Letras minusculas):${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Digite o nome da empresa a ser configurada (Utilizar Letras minusculas):${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " instancia_add
+}
+
+get_max_whats() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o numero maximo de conexões que a empresa ${instancia_add} poderá cadastrar:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " max_whats
+}
+
+get_max_user() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o numero maximo de atendentes que a empresa ${instancia_add} poderá cadastrar:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " max_user
 }
 
 get_frontend_url() {
   
   print_banner
-  printf "${WHITE} 💻 Digite o domínio da interface FRONTEND:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Digite o domínio do FRONTEND/PAINEL para a ${instancia_add}:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " frontend_url
-}
-
-get_frontend_port() {
-  
-  print_banner
-  printf "${WHITE} 💻 Digite a porta do FRONTEND para esta instancia; Ex: 3333 ${GRAY_LIGHT}"
-  printf "\n\n"
-  read -p "> " frontend_port
 }
 
 get_backend_url() {
   
   print_banner
-  printf "${WHITE} 💻 Digite o domínio do BACKEND:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Digite o domínio do BACKEND/API para a ${instancia_add}:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " backend_url
 }
 
+get_frontend_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do FRONTEND para a ${instancia_add}; Ex: 3000 A 3999 ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " frontend_port
+}
+
+
 get_backend_port() {
   
   print_banner
-  printf "${WHITE} 💻 Digite a porta do BACKEND para esta instancia; Ex: 8080 ${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Digite a porta do BACKEND para esta instancia; Ex: 4000 A 4999 ${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " backend_port
 }
 
+get_redis_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do REDIS / AGENDAMENTO MSG para a ${instancia_add}; Ex: 5000 A 5999 ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " redis_port
+}
+
+
 get_urls() {
+  get_mysql_root_password
+  get_link_git
   get_instancia_add
+  get_max_whats
+  get_max_user
   get_frontend_url
   get_backend_url
   get_frontend_port
   get_backend_port
+  get_redis_port
 }
 
 software_update() {
-  
+  get_instancia_add
   frontend_update
   backend_update
 }
@@ -57,10 +104,10 @@ software_update() {
 inquiry_options() {
   
   print_banner
-  printf "${WHITE} 💻 Escolha uma das opções!${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Bem vindo(a) ao instalador Owen Zap, Selecione abaixo a proxima ação!${GRAY_LIGHT}"
   printf "\n\n"
-  printf "   [1] Instalar o Sistema\n"
-  printf "   [2] Atualizar o Sistema\n"
+  printf "   [1] Instalar o OwenZap\n"
+  printf "   [2] Atualizar o OwenZap\n"
   printf "\n"
   read -p "> " option
 
