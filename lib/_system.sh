748752c1ccd10dec0c330abb94e9ffc374a33828
Jorge Luiz gomes <52924232+jjluizgomes@users.noreply.github.com>
2022-07-30 01:37:06 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -36,7 +36,7 @@ system_git_clone() {
   sleep 2
 
   sudo su - owenzap <<EOF
-  git clone https://github.com/mateuscdor/whaticket-ownzp /home/owenzap/${instancia_add}/
+  git clone https://github.com/jjluizgomes/novo /home/owenzap/${instancia_add}/
 EOF
 
   sleep 2
